6a324f2f3f2c81af61b3c1f8cf797066d0d37ad5
Takahiro Tada <t.tada@kms3.com>
2022-09-05 14:28:09 +0900

    add libraries.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("The secret number is: {}", secret_number);    //秘密の数字は次の通り: {}
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
