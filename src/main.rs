7b2b370b1546c73c0d5ff175da9b517984230e0c
Takahiro Tada <t.tada@kms3.com>
2022-09-05 21:23:33 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,44 @@
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
+
+
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-    println!("The secret number is: {}", secret_number);    //秘密の数字は次の通り: {}
+    println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
 
-    let mut guess = String::new();
+        println!("Please input your guess.");
+    
+        let mut guess = String::new();
 
-    io::stdin()
+        io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number!");
+                continue;
+            },
+        };    
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
+
 }
